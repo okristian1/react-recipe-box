b0a92962103248219909f932881bfd2fae8c448e
locrin <okristian1@gmail.com>
2017-05-05 15:10:55 +0200

    mintor stylin

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,13 +4,22 @@ import React from 'react';
 
 function RecipeList (props) {
   return (
-    <div>
+    <div className='recipe-container'>
+    <h1>Saved Recipes</h1>
     <ul>
       {props.recipes.map(function(recipe) {
        return (
-          <li className='popular-list'>
-            {recipe}
+         <ul>
+          <li className='recipe-name'>
+              <h2>Name: {recipe[0]} </h2>
           </li>
+            <li className='recipe-ingredients'>
+              <h3>Ingredients: </h3>{recipe[1]}
+          </li>
+            <li className='recipe-instructions'>
+              <h3>Instructions: </h3>{recipe[2]}
+          </li>
+        </ul>
        )
      })}
      </ul>
