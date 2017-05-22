8f303167b011f878f0c9888c254da20a005a402f
locrin <okristian1@gmail.com>
2017-05-22 02:23:04 +0200

    getting closer

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -6,38 +6,55 @@ class EditItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'editing',
-      ingredients: 'editing',
-      instructions: 'editing'
-    };
-    this.handleChange = this.handleChange.bind(this);
+      uuid: '',
+      name: 'edit',
+      ingredients: '',
+      instructions: ''
+    }
   }
 
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+  handleChange = (event) => {
     this.setState({
-      [name]: value
+      [event.target.name]: event.target.value
     });
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('this is:', this);
   }
 
   render() {
     return(
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            onChange={this.handleChange}
-            />
-        </label>
-        <input
-          type="submit"
-          value="Submit"
-          />
-      </form>
+      <div className='recipe-column'>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text"
+              defaultValue={this.props.name}
+              name="name"
+              onChange={this.handleChange} />
+              Recipe <br />
+          </label>
+          <label>
+          <br />
+            <textarea type="text" name="ingredients"
+            defaultValue={this.state.name}
+            onChange={this.handleChange} />
+          <br />
+            Ingredients <br />
+          </label>
+          <label>
+            <textarea type="text" name="instructions"
+            defaultValue={this.state.name}
+            onChange={this.handleChange} />
+          <br />
+            Instructions <br />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        </div>
     )
   }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,13 +15,9 @@ class Layout extends Component {
       instructions: '',
       recipes: old
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     let newRecipe =
       {
         id: uuid.v4(),
@@ -30,20 +26,22 @@ class Layout extends Component {
         instructions: this.state.instructions,
       }
     this.setState({recipes: this.state.recipes.concat(newRecipe)});
+    this.setState({
+      name: '',
+      ingredients: '',
+      instructions: ''
+    });
     updateStorage(newRecipe);
     event.preventDefault();
   }
 
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+  handleChange = (event) => {
     this.setState({
-      [name]: value
+      [event.target.name]: event.target.value
     });
   }
 
-  handleRemove(id) {
+  handleRemove = (id) => {
     const remainder = this.state.recipes.filter((recipe) => {
       if (recipe.id !== id) return recipe;
     });
@@ -51,45 +49,40 @@ class Layout extends Component {
     deleteRecipe(remainder);
   }
 
-  handleEdit(item) {
-    event.preventDefault();
-    const toEdit = this.state.recipes.filter((recipe) => {
-      if (item.id === recipe.id) return recipe;
-    });
-    let editedRecipe =
-      {
-        id: uuid.v4(),
-        name: 'test',
-        ingredients: 'test',
-        instructions: 'test',
-      }
-    const remainder = this.state.recipes.filter((recipe) => {
-      if (recipe.id !== item.id) return recipe;
-    });
-    this.setState({recipes: remainder});
-    this.setState({recipes: this.state.recipes.concat(editedRecipe)});
-    }
+  handleEdit = (props) => {
+      console.log(props.name);
+  }
+
+
 
     render() {
       return (
         <div className='recipe-column'>
           <form onSubmit={this.handleSubmit}>
             <label>
-              <input type="text"
+              <input
+                value={this.state.name}
+                type="text"
                 name="name"
                 onChange={this.handleChange} />
                 Recipe <br />
             </label>
             <label>
             <br />
-              <textarea type="text" name="ingredients"
+              <textarea
+                value={this.state.ingredients}
+                type="text"
+                name="ingredients"
               onChange={this.handleChange} />
             <br />
               Ingredients <br />
             </label>
             <label>
-              <textarea type="text" name="instructions"
-              onChange={this.handleChange} />
+              <textarea
+                value={this.state.instructions}
+                type="text"
+                name="instructions"
+                onChange={this.handleChange} />
             <br />
               Instructions <br />
             </label>
@@ -101,10 +94,7 @@ class Layout extends Component {
             handleRemove={this.handleRemove}
             handleEdit={this.handleEdit}
           />
-          <EditItem
-            name={this.state.name}
-          />
-
+          <EditItem />
           </div>
       )
     }
